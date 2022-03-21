1bc9182812c9a59012ca77d8dc7814ef2de478ff
James <jamesthao32@gmail.com>
2022-03-21 02:10:41 -0600

    commit tree e20983b2ca0094b992a6aaeafe090b6bcb35d602

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { color } = require("mocha/lib/reporters/base");
+
 const farmAnimals = 'cow horse sheep pig chicken';
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
@@ -32,17 +34,27 @@ const nestedMuppet = {
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
 
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
+const [blackAndWhite, , black, pink ] = farmAnimals.split(" ")
+
 
 // Arrays
 
 // 4. Use destructuring to assign appropriate variables using the color names.
+const [red, orange, yellow, green, blue, indigo, violet] = colors
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
-
+const [r, o, y, g, b, i, v] = colors
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
-
+const [ , , , , , indg, ] = colors
 // Objects
 
 // 7. Use destructuring to assign all variables using the keys as the variable names
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+const {song_2, song_4} = nestedMuppet.album.theMuppetMovie
+const {k_job, k_partner} = nestedMuppet.nestedJob[0,1]
+
+
+
+//const {name, color, song, job, partner} = muppet
+// const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
